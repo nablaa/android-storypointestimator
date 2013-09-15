4ea5e2c824d02f5e7ec0a56b52efdcce91c8287a
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-09-15 21:36:56 +0300

    First implementation of estimator
    Pressing a button on the UI will give a story point estimate. The
    estimate is a random Fibonacci number smaller than 40.

--- src/clojure/org/stuff/storypointestimator/main.clj
+++ src/clojure/org/stuff/storypointestimator/main.clj
@@ -1,8 +1,29 @@
 (ns org.stuff.storypointestimator.main
   (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.notify :only [toast]]
         [neko.threading :only [on-ui]]
         [neko.ui :only [make-ui]]
         [neko.application :only [defapplication]]))
+
+(def ^:const MAX-POINT-VALUE 40)
+
+(defn next-fibonacci [l]
+  (+ (nth l (- (count l) 1))
+     (nth l (- (count l) 2))))
+
+(defn fibonaccies-smaller-than-generated [n generated]
+  (if (> (next-fibonacci generated) n)
+    generated
+    (fibonaccies-smaller-than-generated n (concat generated (list (next-fibonacci generated))))))
+
+(defn fibonaccies-smaller-than [n]
+  (fibonaccies-smaller-than-generated n [1 1]))
+
+(defn get-story-point-estimate []
+  (rand-nth (fibonaccies-smaller-than MAX-POINT-VALUE)))
+
+(defn notify-estimate [_]
+  (toast (str (get-story-point-estimate)) :long))
 
 (defactivity org.stuff.storypointestimator.MainActivity
   :def a
@@ -11,4 +32,5 @@
     (on-ui
      (set-content-view! a
       (make-ui [:linear-layout {}
-                [:text-view {:text "Hello from Clojure!"}]])))))
+                [:button {:text "Estimate"
+                          :on-click notify-estimate}]])))))
